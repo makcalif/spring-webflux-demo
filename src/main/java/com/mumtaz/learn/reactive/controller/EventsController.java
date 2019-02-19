26936c171bb20b972e99178d692b8f0ed7f36b80
Mumtaz Khan <makcalif@yahoo.com>
2019-02-18 23:10:52 -0600

    added CORS to infinite events

--- src/main/java/com/mumtaz/learn/reactive/controller/EventsController.java
+++ src/main/java/com/mumtaz/learn/reactive/controller/EventsController.java
@@ -2,6 +2,7 @@ package com.mumtaz.learn.reactive.controller;
 
 import com.mumtaz.learn.reactive.domain.Event;
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
@@ -15,6 +16,7 @@ import java.util.concurrent.ThreadLocalRandom;
 @RestController
 public class EventsController {
 
+    @CrossOrigin(origins = "http://localhost:4200")
     @GetMapping(name = "/events", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
     public Flux<Event> getUnlimitedEvents () {
         Flux events = Flux.generate(sink -> sink.next(getNextEvent()));
@@ -27,7 +29,9 @@ public class EventsController {
     private Event getNextEvent() {
         List<String> eventTitles = Arrays.asList("Team building",
                                                 "New employee orientation",
-                "Lunch and learn", "Holiday Party", "Cloudfoundry training");
+                                                "Lunch and learn",
+                                                "Holiday Party",
+                                                "Cloudfoundry training");
 
         int next = ThreadLocalRandom.current().nextInt(0, eventTitles.size());
         Event event = new Event(eventTitles.get(next), new Date());
